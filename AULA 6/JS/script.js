2d8039c175542c55284d7c7706454dd6958347a9
joao victor <joao.graciani2003@gmail.com>
2026-02-04 22:15:21 -0300

    AULA 1 JAVASCRIPT

---
+++ AULA 6/JS/script.js
@@ -0,0 +1,109 @@
+// var idade =22
+// console.log(idade)
+// console.log(typeof idade)
+// var nome = "Joao"
+// console.log(typeof nome)
+// var valor = 35.50
+// console.log(typeof valor)
+// var flag = true
+// console.log(typeof flag)
+// var frase = nome+idade+valor
+// console.log(frase)
+// var numero_1 ="50"
+// var numero_2 ="25"
+// console.log(typeof numero_1)
+// console.log(typeof numero_2)
+// total = numero_1- numero_2
+// console.log (total)
+// console.log(typeof total)
+
+// if(valor<18){
+//     console.log("Usuario maior de idade")
+// } else {
+//     console.log("Usuario menor de idade")
+// }
+
+// var idade = Number(prompt("Qual a tua idade? "))
+// console.log(typeof idade)
+
+// if(idade <0 || idade > 100){
+//     console.log("Idade invalida")
+// }
+// else if(idade >= 18){
+//     console.log("Usuario maior de idade")
+// } 
+// else {
+//     console.log("Usuario menor de idade")
+// }
+
+var fruta = "maçã"
+var fruta = "pera"
+let numero = 5
+numero = 6
+console.log(numero)
+
+if(true) {
+    let aluno = "Joao"
+    console.log(aluno)
+}
+
+//variaveis no formato snake_case
+var nome_aluno = "Joao"
+console.log(nome_aluno)
+
+//camel case
+// var nomeAluno = "Maria"
+// console.log(nomeAluno)
+
+// const trabalhador = "Jack"
+// console.log(trabalhador)
+
+// let $nome = "Jimmy"
+// console.log($nome)
+// let numero_3
+// console.log(numero_3)
+// let total_2 = 4/0
+// console.log(total_2)
+// let valor_2 = "80"
+// let unidade = "kg"
+// var peso = valor_2+unidade
+// console.log(peso)
+// let divisao = peso/2
+// console.log(divisao)
+
+// function areaQuadrado(lado) {
+    
+//     return lado * lado 
+// }
+
+// console.log(areaQuadrado(5))
+
+// function PI(){
+//     return 3.14
+// }
+
+// let calculo = 10 * PI()
+// console.log(calculo)
+
+// let peso = Number(prompt("Qual o seu peso em KG?"));
+// let altura = Number(prompt("Qual a sua altura em Mts?"));
+
+// function calcularIMC(peso, altura) {
+//     return peso/(altura**2);
+// }
+
+// let resultado = calcularIMC(peso, altura);
+// console.log("Seu IMC é: " + resultado);
+
+function endereco(rua, numeroo, bairro){
+    return rua+" "+numero+" "+bairro
+}
+
+let rua = prompt("Qual a rua que você mora?")
+
+let numeroo = prompt("Qual o numero da tua moradia?")
+
+let bairro = prompt("Qual o bairro que você mora?")
+
+console.log(endereco(rua,numeroo,bairro))
+
