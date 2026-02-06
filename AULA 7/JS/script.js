d0cf86ad22f4f9d88d001eebe6eb30f51df1033e
joao victor <joao.graciani2003@gmail.com>
2026-02-05 22:17:30 -0300

    terceira atualizaçao

---
+++ AULA 7/JS/script.js
@@ -0,0 +1,58 @@
+// let variavel
+// console.log(variavel)
+
+// var numero = Number(prompt("Digite um numero para saber se é Par ou Impar? "))
+
+// if(numero==0){
+//     console.log("Digitou Zero")
+// }
+// else if(numero % 2 ==0){
+//     console.log("Numero Par")
+// }else{
+//     console.log("Numero Impar")
+// }
+
+// numero = 5
+// // numero = numero + 1
+// numero += 1
+
+// let numero = 5
+// console.log(numero++)
+// let numero_1 = 5
+// console.log(++numero_1)
+
+// let pessoa = {
+//     nome: "Joao",
+//     idade: 22,
+//     cidade:"Curitiba"
+// }
+
+// let quadrado = {
+//     lado: 4,
+//     area: function(lado){
+//         return lado * lado
+//     },
+//     perimetro: function(lado) {
+//         return lado*this.lado
+//     }
+// }
+
+// let quadrado = {
+//     lado: 4,
+//     area(lado){
+//         return lado * lado
+//     },
+//     perimetro(lado) {
+//         return lado*this.lado
+//     }
+// }
+
+
+const titulo = document.getElementById("titulo-principal")
+console.log(titulo.innerHTML)
+
+const titulo2 = document.querySelector('.subtitulo')
+console.log(titulo2.innerHTML)
+
+const home = document.querySelector('.item-menu')
+console.log(home.innerHTML)
